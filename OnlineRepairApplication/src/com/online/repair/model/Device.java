fab75c1933566750af709d9194f58135a2614b20
baddamreddi45 <89807400+baddamreddi45@users.noreply.github.com>
2021-10-14 22:50:21 -0500

    Add files via upload
    a

--- OnlineRepairApplication/src/com/online/repair/model/Device.java
+++ OnlineRepairApplication/src/com/online/repair/model/Device.java
@@ -2,6 +2,12 @@ package com.online.repair.model;
 
 import java.util.Date;
 
+import com.online.repair.builder.DeviceBuilder;
+import com.online.repair.state.CompleteRepairState;
+import com.online.repair.state.RepairInProgressState;
+import com.online.repair.state.RepairState;
+import com.online.repair.state.StartRepairState;
+
 public abstract class Device {
 
 	protected String device;
@@ -10,6 +16,26 @@ public abstract class Device {
 	protected String contactDetails;
 	protected Date transactionDate;
 	protected Date estimatedDeliveryDate;
+
+	protected DeviceBuilder builder;
+
+	RepairState state;
+	RepairState startRepairState;
+	RepairState repairInProgressState;
+	RepairState completeRepairState;
+
+	public Device(DeviceBuilder builder) {
+		this.device = builder.getDevice();
+		this.repairType = builder.getRepairType();
+		this.customerName = builder.getCustomerName();
+		this.contactDetails = builder.getContactDetails();
+		this.transactionDate = builder.getTransactionDate();
+		this.estimatedDeliveryDate = builder.getEstimatedDeliveryDate();
+		this.startRepairState = new StartRepairState(this);
+		this.repairInProgressState = new RepairInProgressState(this);
+		this.completeRepairState = new CompleteRepairState(this);
+		this.state = startRepairState;
+	}
 
 	public String getDevice() {
 		return device;
@@ -59,23 +85,66 @@ public abstract class Device {
 		this.estimatedDeliveryDate = estimatedDeliveryDate;
 	}
 
+	public RepairState getState() {
+		return state;
+	}
+
+	public void setState(RepairState state) {
+		this.state = state;
+	}
+
+	public RepairState getStartRepairState() {
+		return startRepairState;
+	}
+
+	public void setStartRepairState(RepairState startRepairState) {
+		this.startRepairState = startRepairState;
+	}
+
+	public RepairState getRepairInProgressState() {
+		return repairInProgressState;
+	}
+
+	public void setRepairInProgressState(RepairState repairInProgressState) {
+		this.repairInProgressState = repairInProgressState;
+	}
+
+	public RepairState getCompleteRepairState() {
+		return completeRepairState;
+	}
+
+	public void setCompleteRepairState(RepairState completeRepairState) {
+		this.completeRepairState = completeRepairState;
+	}
+
+	public void startRepair() {
+		state.startRepair();
+	}
+
+	public void completeRepair() {
+		state.repairInProgress();
+		state.completeRepair();
+	}
+
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Device [device=");
-		builder.append(device);
-		builder.append(", repairType=");
-		builder.append(repairType);
-		builder.append(", customerName=");
-		builder.append(customerName);
-		builder.append(", contactDetails=");
-		builder.append(contactDetails);
-		builder.append(", transactionDate=");
-		builder.append(transactionDate);
-		builder.append(", estimatedDeliveryDate=");
-		builder.append(estimatedDeliveryDate);
-		builder.append("]");
-		return builder.toString();
+		StringBuilder builder2 = new StringBuilder();
+		builder2.append("Device [device=");
+		builder2.append(device);
+		builder2.append(", repairType=");
+		builder2.append(repairType);
+		builder2.append(", customerName=");
+		builder2.append(customerName);
+		builder2.append(", contactDetails=");
+		builder2.append(contactDetails);
+		builder2.append(", transactionDate=");
+		builder2.append(transactionDate);
+		builder2.append(", estimatedDeliveryDate=");
+		builder2.append(estimatedDeliveryDate);
+		builder2.append(", state=");
+		builder2.append(state);
+		builder2.append("]");
+		return builder2.toString();
 	}
 
 }
